6e81fc15f9e313ad934ae2879192c158808c5184
maxijrabmk <maxijra@outlook.com>
2022-11-18 18:46:39 -0300

    Cambios con intento de plataforma móvil

--- BlockBreaker_Ass/programa_de_referencia_juego.asm
+++ BlockBreaker_Ass/programa_de_referencia_juego.asm
@@ -5,90 +5,74 @@
 	dirEntradas:	.word 	0xFFFFD000
 	dirMillis:	.word 	0xFFFFB000
 	platform:	.word	0x0000F000
+	limiteIzq:	.word	0x80000000
+	limiteDer:	.word	0x00000001
 	
 .text
-	jal data
-
-	#Se carga en el 7 seg
-	addi	$t0, $zero, 169
-	lw	$t1,dir7seg
-	sw	$t0, 0($t1)
-	#Se carga en los LEDs
-	addi 	$t0, $zero, 0x00A7
-	lw 	$t1,dirLEDS
-	sw 	$t0, 0($t1)
+	jal data		##Se cargan todos los datos en la memoria donde se necesitan
+	jal drawBlank		##Se carga el frameBuffer con ceros
+	jal drawPlatform	##Se dibuja la plataforma
 	
-	##Se imprime la primera pantalla de VGA
-	addi	$t0, $zero, 1
-	lw 	$t1,dirVGA
-	addi 	$t2,$zero,25
+	##secuencia sencilla para mover la plataforma de izquiera a derecha
+whileMain:
+	jal leerEntradas
+	addi $t0,$zero,100	##Para comparar 100 ms
+	lw $t1,dirMillis	##Se carga la direccion del contador milisegundos
+while100ms:
+	lw $t2,0($t1)
+	bne $t2,$t0,while100ms	##se repite el bucle y se lee otra vez el contador si es que no son iguales a 100
 	
-while:	sw 	$t0, 0($t1)
-	addi 	$t1,$t1,4
-	addi	$t0,$t0,1
-	bne	$t0,$t2,while
-	
-	##Se sale cuando se terminó de escribir el buffer de imagen con los valores de las líneas
-	lw 	$t1,dirMillis
-	sw 	$t0, 0($t1)	##Se escribe algo en el millis para resetear la cuenta
-	addi 	$s0, $zero, 1000	#Se esperará 1 segundo para realizar cambios
-	
-esperar:
-	lw 	$t0, 0($t1)	##Se lee el valor del contador de millis
-	bne 	$t0, $s0,esperar	##Se espera hasta que los millis sean 1 segundo
-	sw 	$t0, 0($t1)	##Se escribe algo en el millis para resetear la cuenta
-	
-	#Se cargan los valores iniciales
-	addi	$t1,$zero,0	#en t1 el valor para el 7seg
-	addi	$t2,$zero,0x00A7	#en t2 para los LED
-	addi	$t3, $t1,24	#en t3 para el VGA
-	add 	$t4,$zero,$zero	#en t4 la ubi de la linea del vga
-	add	$t5,$zero,$zero	#en t5 la lectura de los botones
+	j whileMain		##Se repite el main
 	
 	
-	##Se leen las entradas
-	##Primero el 7 segmentos
-bucle:	lw 	$t0,dir7seg
-	sw	$t1, 0($t0)
-	addi 	$t1,$t1,1
-	##Segundo, los LEDs
-	andi 	$t6,$t1,0x0001
-	beq 	$t6,$zero,shl
 	
-shr:	sll	$t2,$t2,3
-	j 	next
+leerEntradas:
+	lw $t9,dirEntradas	##Se carga la direccion de las entradas
+	lw $t9,0($t9)		##Se cargan las entradas teniendo la dirección
+	lw $t8,dirVGA		##Se carga la dirección del VGA
+	lw $t7,92($t8)		##Se carga la línea de la plataforma
+	andi $t0,$t9,1		##se hace un andi entre las entradas y la posición 1
+	beq $t0,$zero,next1	##Si es igual a cero, entonces no se presionó izquierda y se busca otro botón
+		lw $t6,limiteIzq
+		and $t6,$t6,$t7	##Se hace un and entre el limite izquierdo y la plataforma para ver si ya se llegó al límite
+		bne $t6,$zero,finLeerEntradas	##Se salta al final si no es cero: la plataforma está al limite izquierdo
+			sll $t7,$t7,1
+			j finLeerEntradas	##Se terminó de hacer los cambios necesarios
+next1:
+	andi $t0,$t9,2		##se hace un andi entre las entradas y la posición 2
+	beq $t0,$zero,next2	##Si es igual a cero, entonces no se presionó derecha y se busca otro botón
+		lw $t6,limiteDer
+		and $t6,$t6,$t7	##Se hace un and entre el limite derecho y la plataforma para ver si ya se llegó al límite
+		bne $t6,$zero,finLeerEntradas	##Se salta al final si no es cero: la plataforma está al limite derecho
+			srl $t7,$t7,1
+			j finLeerEntradas	##Se terminó de hacer los cambios necesarios
+next2:
+	##Si no se presionó ningún botón para mover la plataforma o no se puede más mover, ésta permanece intacta
+finLeerEntradas:
+	sw $t7,92($t8)	##Se escribe la nueva plataforma en la imagen
+	jr $ra		##Se retorna al main
 	
-shl:	srl 	$t2,$t2,3
-
-next:	lw 	$t0,dirLEDS
-	sw 	$t2, 0($t0)
-	##Tercero, el VGA
-	lw 	$t0,dirVGA
-	#add $t0,$t4,$zero
-	lw 	$t7,dirEntradas
-	lw	$t5,0($t7)
-	beq 	$t5,$zero,opc1
 	
-opc0:	sw 	$t5,0($t0)
-	j 	fine
 	
-opc1:	addi 	$t3,$t1,23
-	sw	$t3,0($t0)
+drawPlatform:
+	lw $t0,dirVGA		##Direccion del VGA (primera línea)
+	lw $t1,platform
+	sw $t1,92($t0)		##Se escribe la plataforma en la última línea
+	jr $ra
 	
-fine:	addi 	$t8,$zero,92
-	beq 	$t8,$t4,reset
-	
-noreset:addi 	$t4,$t4,4
-	j 	esperar1seg
-	
-reset:	add 	$t4,$zero,$zero
-
-esperar1seg:
-	lw 	$t0,dirMillis
-	lw 	$t9,0($t0)
-	bne 	$t9, $s0,esperar1seg	##Se espera hasta que los millis sean 1 segundo
-	sw 	$t0, 0($t0)	##Se escribe algo en el millis para resetear la cuenta
-	j bucle
+drawBlank:
+	lw $t0,dirVGA		##Direccion del VGA (primera línea)
+	addi $t1,$zero,24	##lineas totales
+	addi $t2,$zero,0	##linea actual
+	whileDrawBlank:
+		beq $t2,$t1,finWhileDrawBlank
+		sw $zero,0($t0)
+		addi $t0,$t0,4
+		addi $t2,$t2,1
+		j whileDrawBlank
+finWhileDrawBlank:
+	jr $ra
+		
 	
 data:
 	##Segmento donde se cargan las constantes a la memoria
@@ -123,10 +107,22 @@ data:
 	ori 	$t1, $t1, 0xB000 
 	sw 	$t1,0($t0)
 	
-	# platform:	.word	0x0000F000
+	# platform:	.word	0x0007E000
+	addi 	$t0,$t0,4
+	lui 	$t1,0x0007
+	ori 	$t1, $t1, 0xE000
+	sw 	$t1,0($t0)
+	
+	# limiteizq:	.word	0x80000000
+	addi 	$t0,$t0,4
+	lui 	$t1,0x8000
+	ori 	$t1, $t1, 0x0000
+	sw 	$t1,0($t0)
+	
+	# limiteder:	.word	0x00000001
 	addi 	$t0,$t0,4
 	lui 	$t1,0x0000
-	ori 	$t1, $t1, 0xF000 
+	ori 	$t1, $t1, 0x0001
 	sw 	$t1,0($t0)
 
 	jr	$ra
